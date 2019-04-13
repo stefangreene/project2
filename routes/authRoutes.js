5d41f91cef95f94dddad63b1d6252f2796b91e8b
yohan2127 <zedamek@yahoo.com>
2019-04-12 23:01:06 -0400

    update for import

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,4 @@
+var db = require("../models");
 module.exports = function(app, passport, user) {
   var User = user;
 
@@ -48,6 +49,34 @@ module.exports = function(app, passport, user) {
       res.json(dbTodo);
     });
   });
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   app.get("/logout", function(req, res) {
